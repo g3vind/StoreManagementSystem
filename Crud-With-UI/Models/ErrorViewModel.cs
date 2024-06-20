1e7632be3a824b129ceea88786fc5e528e99bc7a
Govind Qmanja <govind@qmanja.com>
2024-06-20 20:24:20 +0530

    Add project files.

---
+++ Crud-With-UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Crud_With_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
