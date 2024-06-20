3704f89357590b5cf3f9dd966a5ec16e02315ac9
Govind Kumar <g3vind@gmail.com>
2024-06-20 22:08:35 +0530

    Read Products and render on products page

---
+++ Crud-With-UI/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Crud_With_UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_With_UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
